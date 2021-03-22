51c048f056cb373e5d3961525518ecf5f40011b6
Karan Bajwa <v-bajkar@microsoft.com>
2021-03-22 13:16:01 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="displayItems">
+     {Array.from({length: 9}, (item, index) => index).map(starId =>
+      <div key={starId} className="star"></div>
+      )}
+      </div>
+     <div className="displayItems">
+     {Array.from({length: 9}, (item, index) => index).map(number =>
+      <div key={number} className="number">{number + 1}</div>
+      )}
+      </div>
+  </div>
   );
 }
 
